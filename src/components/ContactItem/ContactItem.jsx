68751e14e1fa483530696a1998e005dc91bf4b75
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-11-02 15:10:55 +0200

    ContactItem

--- src/components/ContactItem/ContactItem.jsx
+++
@@ -1,8 +0,0 @@
-import { Contact, ButtonRemove } from "./ContactItem.styled";
-
-export const ContactItem = ({ id, name, number, onRemoveContact }) => (
-    <Contact key={id}>
-        <p>{name}: {number}</p>
-        <ButtonRemove type="button" onClick={() => onRemoveContact(id)}>Delete</ButtonRemove>
-    </Contact>
-);
