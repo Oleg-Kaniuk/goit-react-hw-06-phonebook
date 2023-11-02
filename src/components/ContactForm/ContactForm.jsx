1203e42bc80d4dc96cbce2857265ccfd2a12cd2e
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-11-02 13:28:11 +0200

    redux files

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,31 +1,24 @@
-// Імпортуючи useState з бібліотеки React для управління станом у функціональному компоненті
 import { useState } from "react";
-
-// Імпортуючи стилізовані компоненти форми з вказаного файлу
 import { Form, Input, Label, ButtonAdd } from "./ContactForm.styled";
 
-// Визначаємо компонент форми для додавання контактів
 export const ContactForm = ({ onSubmit }) => {
-    // Використання useState для управління станом ім'я та номеру телефону контакту
+    
     const [name, setName] = useState('');
     const [number, setNumber] = useState('');
 
-    // Функція для обробки події відправлення форми
+    
     const onSubmitAddContact = (evt) => {
-        // Забороняємо дефолтну поведінку форми
         evt.preventDefault();
-        // Створюємо об'єкт з даними про контакт та передаємо його до батьківського компонента
+        
         const data = { name, number };
         onSubmit(data);
-        // Очищуємо стан ім'я та номеру
+        
         reset();
     };
 
-    // Функція для обробки зміни значення в input'і
     const onChangeInput = (evt) => {
-        // Деструктуризація події для отримання імені та значення input'а
         const { name, value } = evt.currentTarget;
-        // В залежності від імені поля оновлюємо відповідний стан
+        
         switch (name) {
             case 'name':
                 setName(value);
@@ -38,7 +31,6 @@ export const ContactForm = ({ onSubmit }) => {
         };
     };
 
-    // Функція для скидання стану ім'я та номеру
     const reset = () => {
         setName('');
         setNumber('');
