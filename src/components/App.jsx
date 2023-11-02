b1077bcbb31eccd27999b747c86b835b23d51d31
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-11-02 14:06:18 +0200

    filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -103,3 +103,18 @@ export const App = () => {
     </MainContainer>
   );
 };
+
+// import { ContactForm } from "./ContactForm/ContactForm";
+// import { ContactList } from "./ContactList/ContactList";
+// import { Filter } from "./Filter/Filter";
+// import { MainContainer } from "./App.styled";
+
+// export const App = () => (
+//   <MainContainer>
+//       <h1>Phonebook</h1>
+//       <ContactForm />
+//       <h2>Contacts</h2>
+//       <Filter />
+//       <ContactList />
+//     </MainContainer>
+// )
