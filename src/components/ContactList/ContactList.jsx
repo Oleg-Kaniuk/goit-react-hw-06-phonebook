a3b05a69e9736c72b48209388514934c7c353956
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-11-02 15:06:47 +0200

    ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,28 @@
-import { ContactItem } from "components/ContactItem/ContactItem";
-import { ContactsList } from "./ContactList.styled";
+import { getFilter } from "redux/filterSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { delContact, getPhoneBookValue } from "redux/phoneBookSlice";
+import { ContactsList, Contact, ButtonRemove } from "./ContactList.styled";
 
-export const ContactList = ({ contacts, onRemoveContact }) => (
-    <ContactsList>
-         {contacts.map(contact => (
-             <ContactItem key={contact.id} name={contact.name} number={contact.number} id={contact.id} onRemoveContact={onRemoveContact} />
-        ))}
-    </ContactsList>
-);
+export const ContactList = () => {
+    const dispatch = useDispatch();
+    const phoneBook = useSelector(getPhoneBookValue);
+    const filterPhoneBook = useSelector(getFilter);
+
+    const lowerFilter = filterPhoneBook.toLowerCase().trim();
+    const visibleContacts = phoneBook.filter(({ name }) =>
+        (name.toLowerCase().includes(lowerFilter)));
+    
+    const deleteContact = (contactId) => {
+        dispatch(delContact(contactId))
+    };
+
+    return (
+        <ContactsList>
+            {visibleContacts.map(({ name, number, id }) => (
+                <Contact key={id}>
+                    <p>{name}: {number}</p>
+                    <ButtonRemove type="button" onClick={() => deleteContact(id)}>Delete</ButtonRemove>
+                </Contact>))}
+        </ContactsList>
+    );
+};
--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -7,3 +7,25 @@ export const ContactsList = styled.ul`
     border-radius: 15px;
     font-size: 20px;
 `;
+
+export const Contact = styled.li`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 400px;
+    padding: 10px;
+    border-bottom: 1px solid skyblue;
+`;
+
+export const ButtonRemove = styled.button`
+    height: 30px;
+    width: 100px;
+    padding: 4px;
+    border: 1px solid skyblue;
+    border-radius: 10px;
+    font-weight: 500px;
+    background-color: skyblue;
+    &:hover {
+        background-color: lightblue;
+    }
+`;
